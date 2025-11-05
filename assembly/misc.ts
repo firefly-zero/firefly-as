ca4e1d0b2c293790ebc141e355e52669c647fafe
gram <git@orsinium.dev>
2025-11-05 11:59:04 +0100

    setSeed, getRandom, quit

--- assembly/misc.ts
+++ assembly/misc.ts
@@ -5,3 +5,20 @@ export function logDebug(t: string): void {
   let utf8 = toUtf8(t);
   B.log_debug(strAddr(utf8), strSize(utf8));
 }
+
+export function logError(t: string): void {
+  let utf8 = toUtf8(t);
+  B.log_error(strAddr(utf8), strSize(utf8));
+}
+
+export function setSeed(s: u32): void {
+  B.set_seed(s);
+}
+
+export function getRandom(): u32 {
+  return B.get_random();
+}
+
+export function quit(): void {
+  B.quit();
+}
