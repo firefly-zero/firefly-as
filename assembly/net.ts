5823c815e789b89f5336d0a94e601c3be756ac9a
gram <git@orsinium.dev>
2025-11-08 10:34:59 +0100

    getMe

--- assembly/net.ts
+++ assembly/net.ts
@@ -1,7 +1,17 @@
+import * as B from "./bindings";
+
 export class Peer {
   val: u8;
 
   constructor(v: u8) {
     this.val = v;
   }
+
+  static combined(): Peer {
+    return new Peer(0xff);
+  }
+}
+
+export function getMe(): Peer {
+  return new Peer(B.get_me());
 }
