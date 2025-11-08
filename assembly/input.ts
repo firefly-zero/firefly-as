4b0338a5730ab88acf65d34b708a6d814bdb5290
gram <git@orsinium.dev>
2025-11-08 18:30:15 +0100

    readButtons

--- assembly/input.ts
+++ assembly/input.ts
@@ -11,6 +11,30 @@ export class Pad {
   }
 }
 
+export class Buttons {
+  s: boolean;
+  e: boolean;
+  w: boolean;
+  n: boolean;
+  menu: boolean;
+
+  constructor(raw: u32) {
+    this.s = hasBitSet(raw, 0);
+    this.e = hasBitSet(raw, 1);
+    this.w = hasBitSet(raw, 2);
+    this.n = hasBitSet(raw, 3);
+    this.menu = hasBitSet(raw, 4);
+  }
+
+  anyPressed(): boolean {
+    return this.s || this.e || this.w || this.n || this.menu;
+  }
+}
+
+function hasBitSet(val: u32, bit: u32): boolean {
+  return ((val >> bit) & 1) != 0;
+}
+
 export function readPad(p: Peer): Pad | null {
   const raw = B.read_pad(p.raw);
   const pressed = raw != 0xffff;
@@ -18,4 +42,9 @@ export function readPad(p: Peer): Pad | null {
     return null;
   }
   return new Pad(i16(raw >> 16), i16(raw));
+}
+
+export function readButtons(p: Peer): Buttons {
+  const raw = B.read_buttons(p.raw);
+  return new Buttons(raw);
 }
